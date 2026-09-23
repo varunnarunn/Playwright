480d00755fac71468f850843c53b555344a178f7
VENUKRISHNAN <833416@cognizant.com>
2026-09-23 15:31:17 +0100

    Sharding config

--- tests/sharding.spec.ts
+++ tests/sharding.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 for (let i=0; i <=20 ; i++) {
 
-    test('Practise sharding', async({page}) => {
+    test(`practise shards ${i}`, async({page}) => {
         await page.goto('https://playwright.dev/')
         await expect(page).toHaveTitle(/Playwright/)
     })
